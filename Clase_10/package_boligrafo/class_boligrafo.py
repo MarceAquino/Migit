0843b01d7958d2f7bb2ce9d3dd2bab4690616174
Marcelo Aquino <Marceloaquino1676@gmail.com>
2024-05-16 20:58:47 -0300

    ds

--- Clase_10/package_boligrafo/class_boligrafo.py
+++ Clase_10/package_boligrafo/class_boligrafo.py
@@ -15,36 +15,43 @@ class Boligrafo:
         elif self.color == "verde":
             return Fore.GREEN
         else:
-            return ""       
+            return ""    
+           
     def estilo_grosor(self) -> str:
         if self.grosor_punta == "grueso":
             return Style.BRIGHT
         else:
-            return ""
-            
+            return ""      
+          
     def escribir(self, texto: str) -> str:
+
         longitud = len(texto)
         texto_con_color = f"{self.estilo_grosor()}{self.estilo_color()}{texto}{Style.RESET_ALL}"
         
         if self.cantidad_tinta >= longitud:
             self.cantidad_tinta -= longitud
             respuesta = f"{texto_con_color} - La longitud del texto es de {longitud} caracteres. La tinta restante es {self.cantidad_tinta}%"
+
         else:
-            respuesta = f"No hay suficiente tinta. La cantidad de tinta actual es {self.cantidad_tinta}%. El texto ingresado tiene {longitud} caracteres."
-        
+            respuesta = f"No hay suficiente tinta. La cantidad de tinta actual es {self.cantidad_tinta}%. El texto ingresado tiene {longitud} caracteres."    
         return respuesta
+    
     def recargar(self,cantidad_recargar:str) -> str|int:
+
         cantidad_recargar = int(cantidad_recargar)
         suma_tinta = self.cantidad_tinta+ cantidad_recargar
         if suma_tinta <= self.capacidad_tinta_maxima:
             self.cantidad_tinta = suma_tinta
             respuesta = f"Lapicera recargada el cartucho quedo cargado en un % {self.cantidad_tinta}"
+
         else:
             continuar = input("La cantidad de tinta a recargar es mayor que la capacidad de la lapizera desea continuar si/no : ")
+
             if continuar == "si":
                 diferencia_de_tinta = suma_tinta - self.capacidad_tinta_maxima
                 self.cantidad_tinta = self.capacidad_tinta_maxima
                 respuesta = f"Se recargó la lapicera un % {cantidad_recargar} y sobró  % {diferencia_de_tinta}"
+                
             else:
                 respuesta = "Muchas gracias por por no desperdiciar la tinta que tenga un buen dia." 
 
--- Clase_10/package_boligrafo/main.py
+++ Clase_10/package_boligrafo/main.py
@@ -3,11 +3,11 @@ from class_boligrafo import *
 
 boligrafo_1 = Boligrafo(input("Ingrese el color del bolígrafo (rojo/verde/azul): "),
                         input("Ingrese el grosor del bolígrafo (fino/grueso): "))
-
 texto_a_escribir = input("Ingrese el texto que desea escribir: ")
-print(boligrafo_1.escribir(texto_a_escribir))
 
+print(boligrafo_1.escribir(texto_a_escribir))
 recargar_lapicera = input("¿Desea recargar la lapicera? (si/no): ")
+
 if recargar_lapicera.lower() == "si":
     cantidad_tinta = input("Ingrese la cantidad de tinta a recargar: ")
     print(boligrafo_1.recargar(cantidad_tinta))
