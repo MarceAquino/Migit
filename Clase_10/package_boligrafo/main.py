7d11459102b9863a18977cb4f6e3f460c4a73440
Marcelo Aquino <Marceloaquino1676@gmail.com>
2024-05-16 21:11:05 -0300

    detalles

--- Clase_10/package_boligrafo/main.py
+++ Clase_10/package_boligrafo/main.py
@@ -1,13 +1,22 @@
 from class_boligrafo import * 
 
-
+print("\nBuenos dias, vamos a configurar su boligrafo digital.\n")
 boligrafo_1 = Boligrafo(input("Ingrese el color del bolígrafo (rojo/verde/azul): "),
                         input("Ingrese el grosor del bolígrafo (fino/grueso): "))
+
 texto_a_escribir = input("Ingrese el texto que desea escribir: ")
-
 print(boligrafo_1.escribir(texto_a_escribir))
-recargar_lapicera = input("¿Desea recargar la lapicera? (si/no): ")
+while True:
+    continuar = input("Desea ingresar otro texto si/no : ")
+    if continuar == "si": 
+        texto_a_escribir = input("Ingrese el texto que desea escribir: ")
+        print(boligrafo_1.escribir(texto_a_escribir))
+    else: 
+        break
 
+
+recargar_lapicera = input("¿Desea recargar la lapicera? (si/no): ")
 if recargar_lapicera.lower() == "si":
     cantidad_tinta = input("Ingrese la cantidad de tinta a recargar: ")
     print(boligrafo_1.recargar(cantidad_tinta))
+  
